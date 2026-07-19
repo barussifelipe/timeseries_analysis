67ff2fba52d8e20edaa2c89fcd853ea6ff3e181f
FelipeBarussi <barussifelipe@gmail.com>
2026-07-19 20:08:36 +0200

    data_fetching.py: need to finish the filtering of the NYSE tickers to get only the full shares.

--- data/data_fetching.py
+++ data/data_fetching.py
@@ -13,7 +13,7 @@ conn = sqlite3.connect(db_filename)
 
 cursor = conn.cursor()
 
-def fetch_index_stock(file_path, type="NASDAQ"): 
+def fetch_index_stock(file_path, type: str) -> list: 
     """
     Fetches stock index data from a CSV file and stores it in a SQLite database.
 
@@ -21,15 +21,27 @@ def fetch_index_stock(file_path, type="NASDAQ"):
         file_path (str): The path to the CSV file containing stock index data.
 
     Returns:
-        None
+        list: A list of unique stock symbols.
     """
     # Read the CSV file into a DataFrame
     df = pd.read_csv(file_path, usecols=[0])
 
     if type == "NASDAQ":
         symbols = df.iloc[:, 0].astype(str).str[:4].unique().tolist() 
+
+    elif type == "NYSE":
+        index_string = df.iloc[:, 0].astype(str)
+        symbols = index_string[index_string.str[3] != "^"]
+        symbols = [symbols.drop(symbol) for symbol in symbols if symbol[-2:] == "/B"]
+        
+
+
     
+    print(symbols)
+
     return symbols
+
+
 
 
 
